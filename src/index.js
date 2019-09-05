3f09b8f9711bc77856b41d3c0d4829312497a256
davisc0801 <davisc1014@gmail.com>
2019-09-05 09:45:15 -0600

    Add helper methods to the class to create and cleanup DOM nodes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ class WordWatch(){
     this.apiAddress = "https://wordwatch-api.herokuapp.com"
   }
 
+  cleanup(element){
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+  }
+
+  createNode(element){
+    return document.createElement(element);
+  }
+
   fetchTopWord(){
     fetch(this.apiAddress)
     .then((response) => response.json())
@@ -12,6 +22,12 @@ class WordWatch(){
       this.renderTopWord(topWord);
     })
   }
+
+  renderTopWord(word){
+    let frame = document.getElementById('top-word-frame')
+    this.cleanup(frame)
+  }
+
 }
 
 $(document).ready(() => {
