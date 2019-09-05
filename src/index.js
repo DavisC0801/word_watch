88eaa14931c65c84da17da274e3f81c526116b0e
davisc0801 <davisc1014@gmail.com>
2019-09-05 09:38:15 -0600

    Add a fetch call to the Words Watch API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import $ from 'jquery'
 
+class WordWatch(){
+  constructor(){
+    this.apiAddress = "https://wordwatch-api.herokuapp.com"
+  }
+
+  fetchTopWord(){
+    fetch(this.apiAddress)
+    .then((response) => response.json())
+    .then((topWord) => {
+      this.renderTopWord(topWord);
+    })
+  }
+}
+
 $(document).ready(() => {
   // have fun!
 })
